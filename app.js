1e3107220a8f94547553941e3b59e55782a10ffd
Antony Xavier <antony.xavier@iinerds.com>
2024-03-22 18:20:55 +0530

    removed comments

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import authentication from "./src/Authentication/routes.js";
 import express from "express";
 
-// Create an Express application
 const app = express();
 
-// Define a route
 app.post("/authentication", authentication);
 
-// Start the server
 export default app;
